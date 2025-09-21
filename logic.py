aa5561bddf59376180a70645f9a9758a9e4cb5b9
Lilian <lil.bouhier@gmail.com>
2025-09-21 17:00:52 +0200

    two new functions, one which do the reverse work of the main one, and the other that invert a string

--- logic.py
+++ logic.py
@@ -10,3 +10,31 @@ def convert_binary_to_decimal(binary_number: str) -> int:
         dec += int(bit) * 2 ** pos
         pos -= 1
     return dec
+
+
+def convert_decimal_to_binary(decimal_number: int) -> str:
+    """
+    take a decimal number and convert it to binary (on 8 bits)
+    :param decimal_number: integer (between 0 and 255, verified before using it as function's parameter)
+    :return: string (decimal number converted)
+    """
+    binary = ""
+    while decimal_number > 0:
+        binary += str(decimal_number % 2)
+        decimal_number //= 2
+    for i in range(8 - len(binary)):
+        binary += "0"
+    return reverse(binary)
+
+
+def reverse(chain: str) -> str:
+    """
+    take a string and reverse it
+    :param chain: string
+    :return: chain reversed
+    """
+    pos = -1
+    result = ""
+    for character in range(len(chain)):
+        result += chain[pos - character]
+    return result
